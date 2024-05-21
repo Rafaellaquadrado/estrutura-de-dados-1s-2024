16ce53931e448d4e7803fe2669e1069dbc9394bb
Rafaellaquadrado <lararafaella@gmail.com>
2024-05-21 11:02:34 -0300

    add pilha

---
+++ pilha/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "stack.h"
+
+int main(int argc, char* argv[]){
+
+    StackNode* top = NULL;
+
+    push(&top, 'A');
+    push(&top, 'B');
+    push(&top, 'C');
+
+    display(top);
+
+    printf("%c ", peek(top));
+
+    printf("%c ", pop(&top));
+    printf("%c ", pop(&top));
+ 
+
+    exit(0);
+}
